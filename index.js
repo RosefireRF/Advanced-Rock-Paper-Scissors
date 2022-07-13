51d46d3e3b02b913b6388bb30ca6cdb2e9b7f7da
RosefireRF <57938200+RosefireRF@users.noreply.github.com>
2022-07-13 14:00:08 +0200

    Login GUI
    Implemented visual login as well as a better file structure

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const { SSL_OP_TLS_BLOCK_PADDING_BUG } = require('constants');
 
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static(__dirname + '/public'));
+
 listOfUsers = [];
 listOfPlayers = [];
 listOfRooms = [];
