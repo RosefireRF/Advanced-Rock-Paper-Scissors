c4a6f0a5c169b4514432b0c7ec967b2a9ad7e50f
techbabette <marinakrsticrf@gmail.com>
2022-10-20 19:34:44 +0200

    Object oriented sanitization

--- index.js
+++ index.js
@@ -13,43 +13,54 @@ app.use(express.static(__dirname + '/public'));
 var sRoomId = 0;
 var sPlayerId = 0;
 var sUserId = 0;
-listOfUsers = [];
-listOfPlayers = [];
-listOfRooms = [];
+var possibleMoves = ['swords', 'shields', 'bows'];
+arrayOfUsers = [];
+arrayOfPlayers = [];
+arrayOfRooms = [];
 mapOfUsers = new Map();
 mapOfPlayers = new Map();
+//User class consisting of a socket id, username, move preference and a searchId for the User array
 class User {
-  constructor(id, username, pref, sid){
+  constructor(id, username, pref, searchId){
     this.id = id;
-    this.sid = sid;
+    this.searchId = searchId;
+    //Input sanitization
+    username = sanitize(username);
+    pref = sanitize(pref);
+    //Guard clauses
+    if(username == "" || username.length > 20) username = "Jeremy";
+    if(!pref in possibleMoves) pref = "swords";
     this.username = username;
     this.pref = pref;
   }
 }
+//Player class consisting of the base user information, a room id and a searchId for the Player array
 class Player{
-	constructor(id, username, pref, roomId, sid){
-		this.id = id;
-    this.sid = sid;
+	constructor(baseUser, roomId, searchId){
+		this.id = baseUser.id;
+		this.username = baseUser.username
+    this.pref = baseUser.pref;
+    this.searchId = searchId;
     this.roomId = roomId;
-		this.username = username
 		this.health = 100;
     this.move;
-    this.pref = pref;
 	}
 }
+//Room class consisting of the two players and a searchId for the Room array
 class Room{
-	constructor(Player1, Player2, sid){
-    this.sid = sid;
+	constructor(Player1, Player2, searchId){
+    this.searchId = searchId;
 		this.players = [Player1, Player2];
 		this.name = `${Player1.username} (${Player1.pref}) vs ${Player2.username} (${Player2.pref})`;
     this.finished = 0;
 	}
 }
+//Remove an item from the appropriate array
 function removeFromArray(itemToRemove, array){
-  let index = binarySearch(array, itemToRemove.sid);
+  let index = binarySearch(array, itemToRemove.searchId);
   if(index !== -1) array.splice(index, 1);
 }
-//Sanitize user input
+//Perform binary search on any array given a search id
 function binarySearch(array, id){
   let left = 0;
   let right = array.length - 1;
@@ -57,10 +68,10 @@ function binarySearch(array, id){
   let middle;
   while(left<right){
     middle = Math.floor((left + right) / 2);
-    if(id > array[middle].sid) left = middle+1;
+    if(id > array[middle].searchId) left = middle+1;
     else right = middle;
   }
-  if(array[left].sid === id) return left;
+  if(array[left].searchId === id) return left;
   else return -1;
 }
 function sanitize(string) {
@@ -73,7 +84,7 @@ function sanitize(string) {
       "/": '&#x2F;',
   };
   const reg = /[&<>"'/]/ig;
-  return string.replace(reg, (match)=>(map[match]));
+  return string.replace(reg, (match)=>(map[match])).replace(/\s/g,'');
 }
 //Determine which player won the contested throw
 function checkContestWinner(players){
@@ -109,77 +120,84 @@ function calculateDamage(room, info){
   }
   return;
 }
+//Creating a new room
 function roomCreation(){
-  if(listOfUsers.length < 2){
-    if(listOfUsers.length === 1){
-      io.to(listOfUsers[0].id).emit('waitingEvent');
+  //If amount of users waiting is one, send them a message telling them to wait
+  if(arrayOfUsers.length < 2){
+    if(arrayOfUsers.length === 1){
+      io.to(arrayOfUsers[0].id).emit('waitingEvent');
     }
     return false;
   };
-  let player1 = new Player(listOfUsers[0].id, listOfUsers[0].username, listOfUsers[0].pref, sRoomId, sPlayerId);
-  listOfPlayers.push(player1);
-  mapOfPlayers.set(listOfUsers[0].id, sPlayerId);
+  //Create the first player out of the first user 
+  let player1 = new Player(arrayOfUsers[0], sRoomId, sPlayerId);
+  arrayOfPlayers.push(player1);
+  mapOfPlayers.set(arrayOfUsers[0].id, sPlayerId);
+  //Increment the search player id
   sPlayerId++;
-  let player2 = new Player(listOfUsers[1].id, listOfUsers[1].username, listOfUsers[1].pref, sRoomId, sPlayerId);
-  listOfPlayers.push(player2);
-  mapOfPlayers.set(listOfUsers[1].id, sPlayerId);
+  //Create the second player out of the second user 
+  let player2 = new Player(arrayOfUsers[1], sRoomId, sPlayerId);
+  arrayOfPlayers.push(player2);
+  mapOfPlayers.set(arrayOfUsers[1].id, sPlayerId);
+  //Increment the search player id
   sPlayerId++;
-  listOfUsers.splice(0, 2);
+  //Remove the users turned into players from the users array
+  arrayOfUsers.splice(0, 2);
   console.log("New room created!");
-  console.log(listOfUsers);
+  console.log(arrayOfUsers);
+  //Create new room object containing the two players
   let currentRoom = new Room(player1, player2, sRoomId);
-  listOfRooms.push(currentRoom);
+  arrayOfRooms.push(currentRoom);
   sRoomId++;
   let textValue = `<span class='${player1.pref}-text'>${player1.username}</span> vs <span class='${player2.pref}-text'>${player2.username}</span> `;
+  //Send both players a message letting them know they joined a room
   for (let i = 0; i<2; i++){
-    sid = currentRoom.players[i].id;
-    io.to(sid).emit('roomJoined', {name: textValue} );
+    socketId = currentRoom.players[i].id;
+    io.to(socketId).emit('roomJoined', {name: textValue} );
   }
-  console.log(listOfRooms);
+  console.log(arrayOfRooms);
 }
-let possibleMoves = ['swords', 'shields', 'bows'];
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('setUsername', data =>{
-      let name = sanitize(data.name);
-      let pref = sanitize(data.pref);
-      listOfUsers.push(new User(socket.id, name, pref, sUserId));
+      //When the user selects their username and class, add them to the array of users, increment the searchId for the user array and try to create a room
+      arrayOfUsers.push(new User(socket.id, data.name, data.pref, sUserId));
       mapOfUsers.set(socket.id, sUserId);
       sUserId++;
-      io.to(socket.id).emit('joinEvent', {name: name, pref:pref})
-      console.log(listOfUsers)
+      io.to(socket.id).emit('joinEvent', {name: "You", pref:data.pref});
+      console.log(arrayOfUsers);
       roomCreation();
     });
     socket.on('disconnect', reason =>{
-      //If user is not player, remove from list of users
-      let user = listOfUsers[binarySearch(listOfUsers, mapOfUsers.get(socket.id))];
+      //If the user is not a player, remove them from the array of users
+      let user = arrayOfUsers[binarySearch(arrayOfUsers, mapOfUsers.get(socket.id))];
       if(user) {
-        removeFromArray(user, listOfUsers);
+        removeFromArray(user, arrayOfUsers);
         mapOfUsers.delete(socket.id);
         return};
-      //If user is player, remove from players, delete room and send opponent to queue
-      let player = listOfPlayers[binarySearch(listOfPlayers, mapOfPlayers.get(socket.id))];
+      //If the user is a player, remove them from players, delete their room and send their opponent to the queue
+      let player = arrayOfPlayers[binarySearch(arrayOfPlayers, mapOfPlayers.get(socket.id))];
       if(player){
-        let room = listOfRooms[binarySearch(listOfRooms, player.roomId)];
+        let room = arrayOfRooms[binarySearch(arrayOfRooms, player.roomId)];
         leavingPlayerId = room.players.findIndex(player => player.id === socket.id);
         leavingPlayer = room.players[leavingPlayerId];
         stayingPlayer = room.players[1-leavingPlayerId];
         io.to(stayingPlayer.id).emit('opponentLeft');
-        removeFromArray(leavingPlayer, listOfPlayers);
+        removeFromArray(leavingPlayer, arrayOfPlayers);
         mapOfPlayers.delete(leavingPlayer.id);
-        removeFromArray(stayingPlayer, listOfPlayers);
+        removeFromArray(stayingPlayer, arrayOfPlayers);
         mapOfPlayers.delete(stayingPlayer.id);
-        listOfUsers.push(new User(stayingPlayer.id, stayingPlayer.username, stayingPlayer.pref, sUserId));
+        arrayOfUsers.push(new User(stayingPlayer.id, stayingPlayer.username, stayingPlayer.pref, sUserId));
         mapOfUsers.set(stayingPlayer.id, sUserId);
         sUserId++;
-        removeFromArray(room, listOfRooms)
+        removeFromArray(room, arrayOfRooms)
         roomCreation();
       }
     });
     socket.on('moveSelected', (move) =>{
       //Find player that made move and room where it was made
       console.log("User with ID of " + socket.id + " made move " + move);
-      let player = listOfPlayers[binarySearch(listOfPlayers, mapOfPlayers.get(socket.id))];
+      let player = arrayOfPlayers[binarySearch(arrayOfPlayers, mapOfPlayers.get(socket.id))];
       //Check if user is not player or if user already made a move
       if(!player || player.move){
         console.log("A player tried to make an illegal move");
@@ -188,25 +206,25 @@ io.on('connection', (socket) => {
       if(!move in possibleMoves){
         move = 'swords';
       }
-      let room = listOfRooms[binarySearch(listOfRooms, player.roomId)];
+      let room = arrayOfRooms[binarySearch(arrayOfRooms, player.roomId)];
       console.log(room);
       let players = room.players;
       let movesMade = true;
       player.move = move;
-      console.log(listOfPlayers);
+      console.log(arrayOfPlayers);
       console.log(player.username + "'s move is " + player.move);
       //If either of the players hasn't made a move, the variable is false
       for (let P of players){
         if(typeof P.move == 'undefined')movesMade = false;
       }
+      if(movesMade == true){
       //If both players chose the same move
-      if (movesMade == true){
       if(players[0].move === players[1].move){
         players[0].move = undefined;
         players[1].move = undefined;
         for (let P of players){
-            sid = P.id
-            io.to(sid).emit('moveMade', {type: 2});
+            socketId = P.id
+            io.to(socketId).emit('moveMade', {type: 2});
           }
       }
       //If players chose different moves
@@ -221,36 +239,36 @@ io.on('connection', (socket) => {
         //If the game has ended this round
         if(room.finished === 1){
           for(let i = 0; i<2;i++){
-            sid = players[i].id;
+            socketId = players[i].id;
             text = `<span class='${moves[info.winner]}-text'>${players[info.winner].username}</span> dealt the killing blow to <span class='${moves[1-info.winner]}-text'>${players[1-info.winner].username}</span> winning the game`;
-            io.to(sid).emit('moveMade', {type: info.winner, move:moves[1-i], text:text});
+            io.to(socketId).emit('moveMade', {type: info.winner, move:moves[1-i], text:text});
           }
         }
         //If the game has not ended this round
         if(room.finished === 0){
           for (let i = 0; i<2; i++){
-            sid = players[i].id;
+            socketId = players[i].id;
             if(i === info.winner){
               text = `<span class='${moves[info.winner]}-text'>You</span> did ${info.damage} damage to <span class='${moves[1-info.winner]}-text'>${players[1-info.winner].username}</span> they now have ${players[1-info.winner].health} hp`
             }
             else{
               text = `<span class='${moves[1-info.winner]}-text'>You</span> took ${info.damage} damage from <span class='${moves[info.winner]}-text'>${players[info.winner].username}</span>, you now have ${players[1-info.winner].health} hp`
             }
-            io.to(sid).emit('moveMade', {type: info.winner, move:moves[1-i], text:text});
+            io.to(socketId).emit('moveMade', {type: info.winner, move:moves[1-i], text:text});
           }
         }
         //Process the game ending, send both players to queue;
         if(room.finished === 1){
-          removeFromArray(players[0], listOfPlayers);
-          removeFromArray(players[1], listOfPlayers);
+          removeFromArray(players[0], arrayOfPlayers);
+          removeFromArray(players[1], arrayOfPlayers);
           for (let P of players){
-            listOfUsers.push(new User(P.id, P.username, P.pref, sUserId));
+            arrayOfUsers.push(new User(P.id, P.username, P.pref, sUserId));
             mapOfUsers.set(P.id, sUserId);
             sUserId++;
             mapOfPlayers.delete(P.id);
             roomCreation();
           }
-          removeFromArray(room, listOfRooms);
+          removeFromArray(room, arrayOfRooms);
         }
       }
       }
